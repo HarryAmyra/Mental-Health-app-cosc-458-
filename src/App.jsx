aa4de49ff69d5a2fc9b14051f11107dd0641578b
Myra H <myra@macbookpro.myfiosgateway.com>
2025-11-26 03:48:42 -0500

    Have added the UI for home and wellness tools page. Along wih set up the backend server for the API's. lastly created the chatbot ui and secured the api keyin a env file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
-<<<<<<< HEAD
 import './App.css'
 import Home from './pages/home'
-=======
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import LifeAfterSportsV2 from './pages/LifeAfterSportsV2.jsx';
+import WellnessTools from './pages/wellnessTools.jsx';
 
->>>>>>> 4d44fa73a9039ff3002cc5e1ed66974a899f60fd
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      < Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/life-after-sports" element={<LifeAfterSportsV2 />} />
+        <Route path="/wellnessTools" element={<WellnessTools />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
